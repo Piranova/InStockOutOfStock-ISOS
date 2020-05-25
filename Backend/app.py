d6f5de553cae450295f7973c999d658db8979551
Pavan Kumar <pavan.mt9@gmail.com>
2020-05-25 22:52:20 +0530

    reverted port

--- Backend/app.py
+++ Backend/app.py
@@ -389,4 +389,4 @@ if __name__ == '__main__':
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['PROPAGATE_EXCEPTIONS'] = True
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
